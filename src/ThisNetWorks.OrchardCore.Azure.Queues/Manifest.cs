92afa6fa098dd97c5be6682b4c6740ffe274869f
Dean Marcussen <dean.marcussen@gmail.com>
2020-11-16 10:59:09 +0000

    Project setup

---
+++ src/ThisNetWorks.OrchardCore.Azure.Queues/Manifest.cs
@@ -0,0 +1,15 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "ThisNetWorks OrchardCore Azure Queues",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1"
+)]
+
+[assembly: Feature(
+    Id = "ThisNetWorks.OrchardCore.Azure.Queues",
+    Name = "ThisNetWorks OrchardCore Azure Queues",
+    Description = "Provides features to manage Azure Queues",
+    Category = "Content Management"
+)]
